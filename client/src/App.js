a8b7d88818fe8dec3976111236b71f9d19511c66
manoj sharma <mh19992099@gmail.com>
2022-02-09 12:22:56 +0530

    MERN #19_ Refresh the Page with REACT-ROUTER-DOM

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,97 @@
-import React from 'react';
-import About from './components/About';
-import Contact from './components/Contact';
-import Home from './components/Home';
-import Login from './components/Login';
-import Navbar from './components/Navbar';
-import Register from './components/Register';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Navbar from "./components/Navbar";
+import Register from "./components/Register";
 
 const App = () => {
-  return <div>
-    <Navbar/>
-    <About/>
-    <Contact/>
-    <Register/>
-    <Login/>
-    <Home/>
-  </div>;
+  return (
+    <div>
+      <Router>
+        <Navbar />
+        <Routes>
+        <Route
+            exact
+            path="/about"
+            // key="about"
+            element={
+              <About
+              // setProgress={setProgress}
+              // apiKey={apiKey}
+              // pageSize={page}
+              // country="in"
+              // category="business"
+              />
+            }
+          ></Route>
+           <Route
+            exact
+            path="/contact"
+            // key="about"
+            element={
+              <Contact
+              // setProgress={setProgress}
+              // apiKey={apiKey}
+              // pageSize={page}
+              // country="in"
+              // category="business"
+              />
+            }
+          ></Route>
+           <Route
+            exact
+            path="/register"
+            // key="about"
+            element={
+              <Register
+              // setProgress={setProgress}
+              // apiKey={apiKey}
+              // pageSize={page}
+              // country="in"
+              // category="business"
+              />
+            }
+          ></Route>
+           <Route
+            exact
+            path="/login"
+            // key="about"
+            element={
+              <Login
+              // setProgress={setProgress}
+              // apiKey={apiKey}
+              // pageSize={page}
+              // country="in"
+              // category="business"
+              />
+            }
+          ></Route>
+           <Route
+            exact
+            path="/"
+            // key="about"
+            element={
+              <Home
+              // setProgress={setProgress}
+              // apiKey={apiKey}
+              // pageSize={page}
+              // country="in"
+              // category="business"
+              />
+            }
+          ></Route>
+          {/* <About /> */}
+          {/* <Contact />
+        <Register />
+        <Login />
+        <Home /> */}
+        </Routes>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
-
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return <div>
 
 <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a className="navbar-brand" href="#">Navbar</a>
+  <Link className="navbar-brand" to="/">Navbar</Link>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
@@ -13,23 +14,23 @@ const Navbar = () => {
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
     <ul className="navbar-nav ml-auto">
       <li className="nav-item active">
-        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+        <Link className="nav-link" to="/">Home <span className="sr-only">(current)</span></Link>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="#">About</a>
+        <Link className="nav-link" to="/about">About</Link>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="#">Contact</a>
+        <Link className="nav-link" to="/contact">Contact</Link>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="#">Login</a>
+        <Link className="nav-link" to="/login">Login</Link>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="#">Registration</a>
+        <Link className="nav-link" to="/register">Register</Link>
       </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">About</a>
-      </li>
+      {/* <li className="nav-item">
+        <Link className="nav-link" to="#">About</Link>
+      </li> */}
 
     </ul>
   </div>
